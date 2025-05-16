5d26aa0ce6a02894dc45570d54192e79c7674872
ucrprogrammerFreddy <freddy.sanchezzamora@ucr.ac.cr>
2025-05-16 07:33:55 -0600

    update Administradores.csDto

--- API/DTO/AdministradorDto.cs
+++ API/DTO/AdministradorDto.cs
@@ -37,13 +37,12 @@ namespace PowerVital.DTO
 
         public string Rol { get; set; }
 
+
+  public int Telefono { get; set; }
+
         [Required(ErrorMessage = "El campo titulacion es obligatorio.")] // Valida que el campo no esté vacío.
         [StringLength(100, ErrorMessage = "La titulacion no puede exceder los 100 caracteres.")] // Define la longitud máxima.
         [DataType(DataType.Text)] // Especifica que es un texto.
-
-       
-        public int Telefono { get; set; }
-
         public string? FormacionAcademica { get; set; }
 
 
