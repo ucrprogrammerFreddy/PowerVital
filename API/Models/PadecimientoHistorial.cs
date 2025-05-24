16d131e1e8a9d5ad4a3ca87f8cd69189d8f50176
ucrprogrammerFreddy <freddy.sanchezzamora@ucr.ac.cr>
2025-05-23 18:43:56 -0600

    cambio generar reporte
    se elimino historial Salud y se modifico historialPadecimiento siendo este el unico archivo para manejar historiales

--- API/Models/PadecimientoHistorial.cs
+++ API/Models/PadecimientoHistorial.cs
@@ -1,18 +1,37 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PowerVital.Models
 {
     public class PadecimientoHistorial
     {
         [Key]
-        public int Id { get; set; }
+        public int IdHistorial { get; set; }
 
-        public int HistorialSaludId { get; set; }
-        public HistorialSalud HistorialSalud { get; set; }
+        [Required]
+        public int IdCliente { get; set; }
 
-        public int PadecimientoId { get; set; }
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; set; }
+
+        [Required]
+        public int IdPadecimiento { get; set; }
+
+        [ForeignKey("IdPadecimiento")]
         public Padecimiento Padecimiento { get; set; }
 
-        public string Severidad { get; set; } // Ejemplo: Leve, Moderado, Grave
+        [Required]
+        [StringLength(100)]
+        public string NombrePadecimiento { get; set; } // Copia textual del nombre
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public decimal? Peso { get; set; } // Peso del cliente al momento
+
+        [StringLength(20)]
+        public string Severidad { get; set; } // Leve, Moderado, Grave
+
+    
+
     }
 }
